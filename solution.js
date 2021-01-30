82f7fac6ee6171b1b89427e0fef203e3578420c7
YoniBinder <binderyoni@gmail.com>
2021-01-30 11:51:16 +0200

    Test is over with confirms

--- solution.js
+++ solution.js
@@ -57,80 +57,25 @@ btn.addEventListener("click", function (e) {
     location.reload();
 });
 delAll.addEventListener("click", function (e) {
-    arr = [];
-    localStorage.removeItem("tasks");
-    localStorage.setItem("tasks", JSON.stringify(arr));
-    location.reload();
+    var doDeleteAll = confirm("Delete all tasks?");
+    if (doDeleteAll) {
+        arr = [];
+        localStorage.removeItem("tasks");
+        localStorage.setItem("tasks", JSON.stringify(arr));
+        location.reload();
+    }
 });
 del.addEventListener("click", function (e) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] != null)
-            if (arr[i]["isDone"] == true) {
-                arr.splice(i, 1);
-            }
+    var doDelete = confirm("Delete completed tasks?");
+    if (doDelete) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] != null)
+                if (arr[i]["isDone"] == true) {
+                    arr.splice(i, 1);
+                }
+        }
+        localStorage.removeItem("tasks");
+        localStorage.setItem("tasks", JSON.stringify(arr));
+        location.reload();
     }
-    localStorage.removeItem("tasks");
-    localStorage.setItem("tasks", JSON.stringify(arr));
-    location.reload();
 });
-// let arr:Array<Memo> = []
-// let counter:number =0;
-// let textArea:HTMLInputElement=document.querySelector("#textarea")
-// let date:HTMLInputElement=document.querySelector("#dateInput")
-// let time:HTMLInputElement=document.querySelector("#timeInput")
-// class Memo {
-//   public id: number
-//   public date: string
-//   public time: string
-//   public body: string
-//     constructor (id:number,body:string,date:string,time:string){
-//         this.id = id;
-//         this.date = date;
-//         this.time = time;
-//         this.body = body;
-//     }
-// }
-// const createMemo=():void=>{
-//     let obj = new Memo (counter,textArea.value,date.value ,time.value)
-//     arr.push(obj);
-//     localStorage.setItem('memos',JSON.stringify(arr))
-//     location.reload()
-// }
-// window.onload = (): void => {
-//     if (JSON.parse(localStorage.getItem('memos'))) {
-//       arr = JSON.parse(localStorage.getItem('memos'))
-//       counter = arr.length;
-//       for (let memo of arr) {
-//         if(memo != null) {
-//           let containerEl= document.querySelector(".container")
-//           let memoDiv= document.createElement("div")
-//           memoDiv.classList.add("memo")
-//   memoDiv.innerHTML = `<br><button class="close" onclick="removeMemo(${memo["id"]})">X</button>
-//   <br>${memo["body"]} <br><br> ${memo["date"]} <br> ${memo["time"]}<br>`
-//   // let btn=document.querySelector(".close")
-//           // btn.style.display="none";
-//           // memoDiv.addEventListener("mouseover", ()=>{btn.style.display="initial"})
-//           // memoDiv.addEventListener("mouseout", ()=>{btn.style.display="none"})
-//           containerEl.appendChild(memoDiv)
-//       }
-//     }
-//   }
-// }
-// function removeMemo(key: number): void {
-//   for (let i = 0 ; i < arr.length ; i++) {
-//     if (arr[i] != null) 
-//       if (arr[i]["id"] == key) {
-//         arr.splice(key,1)
-//         break
-//       }
-//   }
-//   console.log(arr);
-//   localStorage.removeItem("memos")
-//   localStorage.setItem("memos", JSON.stringify(arr))
-//   location.reload()
-// }
-// function clear(): void {
-//   textArea.value = ''
-//   date.value = ''
-//   time.value = ''
-// }
